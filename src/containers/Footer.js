dcb16264edd5b9052636ee4f5dc73de92a84e0c4
Aleksandar Vukasinovic <alekvuka@gmail.com>
2021-01-24 22:52:25 -0500

    update site url in footer

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -17,7 +17,7 @@ class Footer extends Component {
     return (
           <Row style={{ backgroundColor: "lightpink", marginTop: "60px"}} className="justify-content-md-center">
             <Col >
-              <h5 style={{color: "purple", fontSize: ".80em", textAlign: 'center', paddingTop:"10px"}}>&hearts; made by <a href='http://www.aleksvuk.com' target="_blank">aleks</a></h5>
+              <h5 style={{color: "purple", fontSize: ".80em", textAlign: 'center', paddingTop:"10px"}}>&hearts; made by <a href='https://aleks-vuk.herokuapp.com/' target="_blank">aleks</a></h5>
             </Col>
           </Row>
     )
